030590324da7c13df83b76906163c73cbb9206bf
candido dessanti <candido.dessanti@hotmail.it>
2017-11-27 16:12:40 +0100

    a briefly reformat

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-import subprocess
 from setuptools import setup, find_packages, Extension
 
 setup(
-  name='pgFDW_mapd',
-  version='0.1',
-  author='Candido Dessanti',
-  license='Mit',
-  packages=['pgFDW_mapd']
+    name='pgFDW_mapd',
+    version='0.1',
+    author='Candido Dessanti',
+    license='Mit',
+    packages=['pgFDW_mapd']
 )
